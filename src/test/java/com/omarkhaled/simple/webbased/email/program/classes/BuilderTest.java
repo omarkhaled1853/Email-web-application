ac665f43d13e710fb729fdb96aad49a0c9e8b3a3
محمد حسن <nhmdh7092@gmail.com>
2023-12-22 23:59:11 +0200

    add unit test for user and mail and attachments classes

--- src/test/java/com/omarkhaled/simple/webbased/email/program/classes/BuilderTest.java
+++ src/test/java/com/omarkhaled/simple/webbased/email/program/classes/BuilderTest.java
@@ -13,8 +13,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 class BuilderTest {
 
     @Test
-    public void testuserbuildind(){
-        var user=new User.Builder()
+    public void testuserbuildind() {
+        var user = new User.Builder()
                 .setEmail("test@CSED.com")
                 .setPassword("23122002")
                 .setUserName("mohamed")
@@ -26,11 +26,11 @@ class BuilderTest {
                 .setSent(new HashMap<>())
                 .build();
         assertNotNull(user);
-        assertEquals("test@CSED.com",user.getEmail());
-        assertEquals("23122002",user.getPassword());
-        assertEquals("mohamed",user.getUserName());
-        assertEquals("test@CSED.com",user.getEmail());
-        assertEquals("test@CSED.com",user.getEmail());
+        assertEquals("test@CSED.com", user.getEmail());
+        assertEquals("23122002", user.getPassword());
+        assertEquals("mohamed", user.getUserName());
+        assertEquals("test@CSED.com", user.getEmail());
+        assertEquals("test@CSED.com", user.getEmail());
         assertNotNull(user.getInbox());
         assertNotNull(user.getSent());
         assertNotNull(user.getDraft());
@@ -39,9 +39,9 @@ class BuilderTest {
         assertNotNull(user.getContacts());
 
     }
+
     @Test
-    public void getmailsusinguseremail()
-    {
+    public void getmailsusinguseremailtestinbox() {
         var mailBuilder = new Mail.Builder();
         List<String> attachments = new ArrayList<>();
         attachments.add("img1");
@@ -49,19 +49,52 @@ class BuilderTest {
         attachments.add("img3");
 
         Map<String, Mail> inbox = new HashMap<>();
-        inbox.put("moahmed", mailBuilder.setId("1").setPriority(5).setSender("mohamed@CSED.com").setReceiver("omar@CSED.com").setContent("test mail builder").build());
+        inbox.put("mohamed", mailBuilder.setId("1").setPriority(5).setSender("mohamed@CSED.com").setReceiver("omar@CSED.com").setContent("test mail builder").build());
         inbox.put("omar", mailBuilder.setId("5").setPriority(5).setSender("ali@CSED.com").setReceiver("yousef@CSED.com").setContent("test mail builder").build());
 
         User user = new User.Builder()
                 .setInbox(inbox)
                 .build();
-
         Mail expectedMail = mailBuilder.setId("8").setPriority(10).setSender("mohamed@CSED.com").setReceiver("omar@CSED.com").setContent("test mail builder").build();
-        System.out.println(inbox.get("moahmed"));
-        ///OUTPUT Mail{id='1', priority=5, sender='mohamed@CSED.com', receiver='omar@CSED.com', content='test mail builder', attachments=[img1, img2, img3]}
-        assertEquals(expectedMail, inbox.get("moahmed"));
+        assertEquals(expectedMail.getReceiver(), inbox.get("mohamed").getReceiver());
+        assertEquals(expectedMail.getSender(), inbox.get("mohamed").getSender());
+        assertEquals(expectedMail.getId(), inbox.get("mohamed").getId());
+        assertEquals(expectedMail.getContent(), inbox.get("mohamed").getContent());
+        assertEquals(expectedMail.getAttachments(), inbox.get("mohamed").getAttachments());
+        assertEquals(expectedMail.getPriority(), inbox.get("mohamed").getPriority());
+        assertEquals(expectedMail.getPriority(), inbox.get("mohamed").getPriority());
     }
+
+    @Test
+    public void getmailsusinguseremailtestSent() {
+        var mailBuilder = new Mail.Builder();
+        List<String> attachments = new ArrayList<>();
+        attachments.add("img1");
+        attachments.add("img2");
+        attachments.add("img3");
+
+        Map<String, Mail> sent = new HashMap<>();
+        sent.put("mohamed", mailBuilder.setId("1").setPriority(5).setSender("mohamed@CSED.com").setReceiver("omar@CSED.com").setContent("test mail builder").build());
+        sent.put("omar", mailBuilder.setId("5").setPriority(5).setSender("ali@CSED.com").setReceiver("yousef@CSED.com").setContent("test mail builder").build());
+
+        User user = new User.Builder()
+                .setSent(sent)
+                .build();
+        System.out.println(sent.get("mohamed").getId());
+        System.out.println("-------------------------------");
+        Mail expectedMail = mailBuilder.setId("8").setPriority(10).setSender("mohamed@CSED.com").setReceiver("omar@CSED.com").setContent("test mail builder").build();
+        System.out.println(expectedMail.getId());
+        System.out.println(sent.get("mohamed").getId());
+        assertEquals(expectedMail.getReceiver(), sent.get("mohamed").getReceiver());
+        assertEquals(expectedMail.getSender(), sent.get("mohamed").getSender());
+        assertEquals(expectedMail.getId(), sent.get("mohamed").getId());
+        assertEquals(expectedMail.getContent(), sent.get("mohamed").getContent());
+        assertEquals(expectedMail.getAttachments(), sent.get("mohamed").getAttachments());
+        assertEquals(expectedMail.getPriority(), sent.get("mohamed").getPriority());
+        assertEquals(expectedMail.getPriority(), sent.get("mohamed").getPriority());
+
     }
+}
 
 
 
